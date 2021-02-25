fd723e7cf79173a8fdfe6f4d70506d1055b9fefa
cyborgmg <cyborgmg@gmail.com>
2021-02-25 09:59:35 -0300

    Login e geração do Token JWT

--- hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -4,11 +4,14 @@ import com.devsuperior.hroauth.entities.User;
 import com.devsuperior.hroauth.feignclientes.UserFeignCliente;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserFeignCliente userFeignCliente;
@@ -24,6 +27,17 @@ public class UserService {
 
         log.info("mail encontrado = {}",email);
 
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userFeignCliente.findByEmail(email).getBody();
+        if(user==null){
+            log.error("mail não encontrado = {}",email);
+            throw new UsernameNotFoundException("email não encontrado");
+        }
+        log.info("mail encontrado = {}",email);
         return user;
     }
 }
